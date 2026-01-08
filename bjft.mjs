e76b440a8a6b150cc53260d192039e38d70f2e1c
Дід Alik <didalik.aim@gmail.com>
2026-01-08 14:59:14 +0000

    Start jf agents

--- bjft.mjs
+++ bjft.mjs
@@ -1,4 +1,4 @@
-import { configuration, wsclient_onmessage, } from '../../local/lib/util.mjs' // {{{1
+import { configuration, bjft_onmessage, } from '../local/lib/util.mjs' // {{{1
 import { put, reset, } from './lib/util.mjs'
 import { connection, } from '../../lib/util.mjs'
 
@@ -6,18 +6,17 @@ reset({ content: document.getElementById('content1'), }) // {{{1
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
 configuration.me = 'Ann' // {{{1
-let msg = "Hi there! I'm Ann."
+//let msg = "Hi there! I'm Ann."
 const out = m => typeof m == 'string' ? put(`<h4 style='text-align: right'>${m}</h4>`)
   : put(m.message)
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
   on('error', console.error).
-  on('message', mobj => wsclient_onmessage(ws, mobj, out)).
+  on('message', mobj => bjft_onmessage(ws, mobj, out)).
   on('close', data => {
     console.log(configuration.me, 'close data', data)
     put("<h3 style='text-align: center'>Test PASSED</h3>")
-  }).
-  send(msg)
+  })//.send(msg)
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
 
-put(`<h4 style='text-align: right'>${msg}</h4>`)
+//put(`<h4 style='text-align: right'>${msg}</h4>`)
