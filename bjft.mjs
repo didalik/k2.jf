2d8ecdbc27f8000a5bf2be994869cd396779adbc
Дід Alik <didalik.aim@gmail.com>
2026-01-11 02:34:24 +0000

    Use https://github.com/feross/buffer

--- bjft.mjs
+++ bjft.mjs
@@ -1,12 +1,14 @@
-import { configuration, bjft_onmessage, } from '../local/lib/util.mjs' // {{{1
+import { JobRequest, configuration, bjft_onmessage, } from '../local/lib/util.mjs' // {{{1
 import { put, reset, } from './lib/util.mjs'
+import { generate_keypair, } from '../lib/util.mjs'
 import { connection, } from '../../lib/util.mjs'
 
 reset({ content: document.getElementById('content1'), }) // {{{1
 put(`Delivered ${location} on ${Date()} to YOUR_IP_ADDRESS`, '<hr/>')
   
 configuration.me = 'Ann' // {{{1
-//let msg = "Hi there! I'm Ann."
+generate_keypair.call(crypto.subtle).then(keys => console.log(keys))
+
 const out = m => typeof m == 'string' ? put(`<h4 style='text-align: right'>${m}</h4>`)
   : put(m.message)
 const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
@@ -15,7 +17,7 @@ const ws = connection(new WebSocket(location.toString().replace('http', 'ws'))).
   on('close', data => {
     console.log(configuration.me, 'close data', data)
     put("<h3 style='text-align: center'>Test PASSED</h3>")
-  })//.send(msg)
+  }).send(JobRequest(`bjft/echo:${configuration.me}`))
 
 setInterval(ws.open, 10000)                // auto-reconnect every 10s
 
